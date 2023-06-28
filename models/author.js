dc0b6fbacaf277d0724c9d2a618f50d4c1b5bb04
Asam Afzal <aainvests@outlook.com>
2023-06-27 20:45:12 -0400

    displaying library data

--- models/author.js
+++ models/author.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const { DateTime } = require("luxon");
 
 const Schema = mongoose.Schema;
 
@@ -20,6 +21,18 @@ AuthorSchema.virtual("name").get(function () {
 
 AuthorSchema.virtual("url").get(function() {
     return `/catalog/author/${this.id}`;
+});
+
+AuthorSchema.virtual("formatted_date_of_birth").get(function () {
+    return this.date_of_birth ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED) : '';
+});
+
+AuthorSchema.virtual("formatted_date_of_death").get(function () {
+    return this.date_of_death ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED) : '';
+});
+
+AuthorSchema.virtual("formatted_life_span").get(function () {
+    return (this.date_of_birth ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED) : '') + " - " + (this.date_of_death ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED) : '');
 });
 
 module.exports = mongoose.model("Author", AuthorSchema);
